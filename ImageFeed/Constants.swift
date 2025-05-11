8913be58a667c54ea6111c76baa1e8066d6c1062
Kirill Serebryannyy <sincerely.manny@gmail.com>
2025-05-11 17:55:04 +0300

    feat: Implement mock server for local development

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -6,5 +6,15 @@ enum Constants {
   static let secretKey = "sKiDGKbD9_-0Ze-nVSlPbe7cbhGT_40PgI77SNSK-Xw"
   static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
   static let defaultBaseURL = URL(string: "https://unsplash.com/")
-  static let apiBaseURL = URL(string: "https://api.unsplash.com/")
+  //static let apiBaseURL = URL(string: "https://api.unsplash.com/")
+  /*
+  Uncomment the line below to use the local server (to avoid rate limits)
+  start the server with:
+  ```sh
+  cd ./MockServer
+  npm install
+  npm start
+  ```
+  */
+  static let apiBaseURL = URL(string: "http://localhost:3000/")
 }
